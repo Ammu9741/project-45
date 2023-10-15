c31e1a3eaa23cdf3b195b5d1ef44c70a93bf653f
Ammu9741 <121373673+Ammu9741@users.noreply.github.com>
2023-10-15 12:24:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,85 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombie, zombieImg
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+zombieImg = loadImage("assets/zombie.png")
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+enemy()
+
+drawSprites();
+
+}
+
+function enemy(){
+
+  if(frameCount%100===0)
+  {
+    x = random(500,1200)
+    y = random(100,500)
+    zombie = createSprite(x,y,50,50)
+    zombie.velocityX = -3
+    zombie.addImage(zombieImg)
+    zombie.scale = 0.15
+
+  }
+}
+
+
+
+
+
+
